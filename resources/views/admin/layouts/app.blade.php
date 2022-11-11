1429f2ffaba5ec7f3e6358387446cd36fed04a8a
JoshiBipin <bipin.ingrails@gmail.com>
2022-11-11 11:30:44 +0545

    dashboard code comment

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,21 +10,21 @@
       <title>Colab | @yield('title')</title>
       <meta name="csrf-token" content="{{ csrf_token() }}" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
-      <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
-      <link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/jqvmap/jqvmap.min.css')}}">
-      <link rel="stylesheet" href="{{asset('css/adminlte.min2167.css?v=3.2.0')}}">
-      <link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/daterangepicker/daterangepicker.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/toastr/toastr.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
-      <link rel="stylesheet" href="{{asset('plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
-      <link rel="stylesheet" href="{{asset('css/style.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/fontawesome-free/css/all.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/css/ionicons.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/jqvmap/jqvmap.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/css/adminlte.min2167.css?v=3.2.0')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/daterangepicker/daterangepicker.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/toastr/toastr.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/summernote/summernote-bs4.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
+      <link rel="stylesheet" href="{{asset('public/css/style.css')}}">
       <script>
          var base_url = window.location.origin;
       </script>
@@ -32,12 +32,12 @@
    <body class="hold-transition sidebar-mini layout-fixed">
       <div class="wrapper">
          <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{asset('img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="{{asset('public/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
          </div>
          @include('admin.components.top-nav')
          <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="/" class="brand-link">
-            <img src="{{asset('img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="{{asset('public/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">Colab</span>
             </a>
             @include('admin.components.sidebar')
@@ -75,29 +75,29 @@
        </div>
    </body>
 
-   <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
-   <script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+   <script src="{{asset('public/plugins/jquery/jquery.min.js')}}"></script>
+   <script src="{{asset('public/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
    <script>
       $.widget.bridge('uibutton', $.ui.button)
    </script>
-   <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-   <script src="{{asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
-   <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
-   <script src="{{asset('js/adminlte2167.js')}}"></script>
-   <script src="{{asset('plugins/toastr/toastr.min.js')}}"></script>
-   <script src="{{asset('plugins/sweetalert2/sweetalert2.min.js')}}"></script>
-   <script src="{{asset('js/editable.js')}}"></script>
-   <!-- <script src="{{asset('plugins/chart.js')}}/Chart.min.js')}}"></script> -->
-   <!-- <script src="{{asset('plugins/sparklines/sparkline.js')}}"></script> -->
-   <!-- <script src="{{asset('plugins/jqvmap/jquery.vmap.min.js')}}"></script> -->
-   <!-- <script src="{{asset('plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script> -->
-   <!-- <script src="{{asset('js/demo.js')}}"></script> -->
-   <!-- <script src="{{asset('plugins/moment/moment.min.js')}}"></script> -->
-   <!-- <script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script> -->
-   <!-- <script src="{{asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script> -->
-   <!-- <script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script> -->
+   <script src="{{asset('public/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+   <script src="{{asset('public/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+   <script src="{{asset('public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+   <script src="{{asset('public/js/adminlte2167.js')}}"></script>
+   <script src="{{asset('public/plugins/toastr/toastr.min.js')}}"></script>
+   <script src="{{asset('public/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
+   <script src="{{asset('public/js/editable.js')}}"></script>
+   <!-- <script src="{{asset('public/plugins/chart.js')}}/Chart.min.js')}}"></script> -->
+   <!-- <script src="{{asset('public/plugins/sparklines/sparkline.js')}}"></script> -->
+   <!-- <script src="{{asset('public/plugins/jqvmap/jquery.vmap.min.js')}}"></script> -->
+   <!-- <script src="{{asset('public/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script> -->
+   <!-- <script src="{{asset('public/js/demo.js')}}"></script> -->
+   <!-- <script src="{{asset('public/plugins/moment/moment.min.js')}}"></script> -->
+   <!-- <script src="{{asset('public/plugins/daterangepicker/daterangepicker.js')}}"></script> -->
+   <!-- <script src="{{asset('public/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script> -->
+   <!-- <script src="{{asset('public/plugins/summernote/summernote-bs4.min.js')}}"></script> -->
    
-   <script src="{{asset('js/pages/dashboard.js')}}"></script>
+   <script src="{{asset('public/js/pages/dashboard.js')}}"></script>
 
    {{-- flash messages --}}
    @if(session()->has('success'))
